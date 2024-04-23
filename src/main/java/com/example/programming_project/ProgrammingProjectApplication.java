89fa6862669fc9f1a75333794fc28be62f1df0b3
Youmni <youmni.malha@gmail.com>
2024-04-23 11:40:44 +0200

    Initial commit

---
+++ src/main/java/com/example/programming_project/ProgrammingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.programming_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgrammingProjectApplication.class, args);
+    }
+
+}
