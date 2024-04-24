8eae60c5a598d7c460eae55701e55dd6b808559c
Youmni <youmni.malha@gmail.com>
2024-04-24 20:51:45 +0200

    Voorbeeld om te zien of de andere team genoten info kunnen fetchen

--- src/main/java/com/example/programming_project/ProgrammingProjectApplication.java
+++ src/main/java/com/example/programming_project/ProgrammingProjectApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProgrammingProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ProgrammingProjectApplication.class, args);
-    }
 
+    }
 }
